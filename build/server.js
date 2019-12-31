121d69e118830411970bc0bca4e7fc8217f73316
Matheus Mayron <matheusmayron8@gmail.com>
2019-12-31 09:57:22 -0300

    Removido antigo diretorio de build.

--- build/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var http = require("http");
-var api_1 = require("./api/api");
-var config = require('./config/env/config')(); //() Invocação imediata
-var server = http.createServer(api_1.default);
-server.listen(config.serverPort);
-server.on('listening', function () { return console.log("Server online on port " + config.serverPort); });
-server.on('error', function (error) { return console.log("Ocorreu um erro: " + error); });
